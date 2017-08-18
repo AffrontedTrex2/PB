ea73505f1a5d64d3e104cb95e6abad19c7358e79
Jasmine Zhang <jasminezhang@live.com>
2017-08-18 11:38:13 -0700

    KMS!!!

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class Health : MonoBehaviour
+{
+
+    public static int startingHealth = 5;
+
+    public static int currentHealth;
+    public Sprite[] heartsLeft;
+    public Image HeartUI;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+        HeartUI.sprite = heartsLeft[currentHealth];
+        Debug.Log("Health: " + currentHealth);
+        for (int i = 0; i < heartsLeft.Length; i++)
+        {
+            Debug.Log(heartsLeft[i]);
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    public void ChangeHealth(int amount)
+    {
+        Debug.Log("ChangeHealth called");
+        Debug.Log("HealthBefore: " + currentHealth);
+        currentHealth += amount;
+        Debug.Log("Health: " + currentHealth);
+        Debug.Log("Length: " + heartsLeft.Length);
+        for (int i = 0; i < heartsLeft.Length; i++)
+        {
+            Debug.Log(heartsLeft[i]);
+        }
+        //HeartUI.sprite = heartsLeft[currentHealth];
+    }
+}
---
+++ Assets/Scripts/PickupCoin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupCoin : MonoBehaviour
+{
+    public GemCount gems; //Reference GemCount.cs, called gems.
+    public bool HasBeenCollected = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Hello");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (HasBeenCollected == false)
+            {
+                HasBeenCollected = true;
+                Destroy(gameObject);
+                gems.ChangeGemCount();
+
+            }
+            
+        }
+    }
+}
